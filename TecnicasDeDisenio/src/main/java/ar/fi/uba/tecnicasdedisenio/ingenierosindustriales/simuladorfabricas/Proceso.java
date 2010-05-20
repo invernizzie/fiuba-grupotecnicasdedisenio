d9014905e5bc29d8055d04dd36230c534c12aa51
gmeller <gmeller@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-05-20 15:33:01 +0000

    Cambios en el disenio del laboratorio, perdon por la desprolijidad de subir cada 2 mins.

--- TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/Proceso.java
+++ TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/Proceso.java
@@ -1,24 +1,51 @@
 package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Proceso {
 	
-	private ArrayList<Paso> pasos;
+	private ArrayList<TipoMaquina> tiposDeMaquinas;
 	
-	public Proceso(){
-		this.setPasos(new ArrayList<Paso>());
-	}
-	
-	public void generarPaso(){
-		this.getPasos().add( new Paso());
+	public Proceso() {
+		this.setMaquinas(new ArrayList<TipoMaquina>());
 	}
 
-	public void setPasos(ArrayList<Paso> pasos) {
-		this.pasos = pasos;
+	public void agregarTipoMaquina(TipoMaquina tipoMaquina){
+		this.getMaquinas().add(tipoMaquina);
 	}
 
-	public ArrayList<Paso> getPasos() {
-		return pasos;
+	public void setMaquinas(ArrayList<TipoMaquina> maquina) {
+		this.tiposDeMaquinas = maquina;
 	}
+
+	public ArrayList<TipoMaquina> getMaquinas() {
+		return tiposDeMaquinas;
+	}
+	
+	public Iterator<TipoMaquina> iterator(){
+		return new IteradorProceso();
+	}
+	
+	public class IteradorProceso implements Iterator<TipoMaquina>{
+		private int indice;
+		
+		public IteradorProceso(){
+			indice = 0;
+		}
+		
+		public boolean hasNext() {
+			return indice<getMaquinas().size();
+		}
+
+		public TipoMaquina next() {
+			return getMaquinas().get(indice);
+		}
+
+		public void remove() {
+			getMaquinas().remove(indice);
+		
+		}
+	}
+	
 }
