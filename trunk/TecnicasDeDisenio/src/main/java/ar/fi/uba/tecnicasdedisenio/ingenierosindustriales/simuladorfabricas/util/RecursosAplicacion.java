80a211789098a1a72d72cddb049ac3d140c5f118
nrisaro <nrisaro@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-05-20 03:19:59 +0000

    Lector de properties desde XML

---
+++ trunk/TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/util/RecursosAplicacion.java
@@ -0,0 +1,70 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Singleton que permite acceso a los recursos globales de la aplicacion
+ * @author santiago
+ *
+ */
+public class RecursosAplicacion {
+	public static final String PROPERTIES_PATH = "config.xml";
+
+	/**
+	 * Properties de la aplicacion
+	 */
+	private Properties properties = null;
+
+	/**
+	 * La unica instancia de este Singleton
+	 */    
+	private static RecursosAplicacion instance = new RecursosAplicacion();
+
+	/** 
+	 * Forzamos el Singleton ocultando el constructor 
+	 */ 
+	private RecursosAplicacion() {
+		this.loadProperties();
+	}
+
+	public static RecursosAplicacion instance(){
+		return instance;
+	}
+	
+	/**
+     * Carga las properties de configuración.
+     *
+     */
+    protected void loadProperties() {
+        
+        try {
+            
+            InputStream is = this.getClass().getClassLoader()
+                                        .getResourceAsStream(PROPERTIES_PATH);
+            
+            this.properties = new Properties();
+            this.properties.loadFromXML(is);
+            
+            
+        } catch (Exception error) {
+            
+        }
+    }
+
+    public Properties getProperties() {
+        return this.properties;
+    }
+    
+    public String getProperty(String key) {
+        return this.getProperties().getProperty(key);
+    }
+
+    
+    public int getIntProperty(String key) {
+        
+        return Integer.parseInt(this.getProperty(key));
+    }
+    
+}
+
---
+++ trunk/TecnicasDeDisenio/src/test/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/tests/util/TestRecursosAplicacion.java
@@ -0,0 +1,35 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.tests.util;
+
+import static org.junit.Assert.fail;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.util.RecursosAplicacion;
+
+public class TestRecursosAplicacion {
+
+	private RecursosAplicacion instanciaRecursosAplicacion;
+	
+	@Before
+	public void setUp() throws Exception {
+		instanciaRecursosAplicacion = RecursosAplicacion.instance();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetProperty() {
+		Assert.assertEquals("No se recuperó la property esperada", "testValue", instanciaRecursosAplicacion.getProperty("testProperty"));
+	}
+
+	@Test
+	public void testGetIntProperty() {
+		Assert.assertEquals("", 1, instanciaRecursosAplicacion.getIntProperty("testIntProperty"));
+	}
+
+}
