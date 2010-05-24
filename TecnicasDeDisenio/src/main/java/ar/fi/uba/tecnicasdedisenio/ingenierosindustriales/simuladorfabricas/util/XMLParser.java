ed390e5d4c9a9430c1e78d7e50a45833fa936e6b
diegogarcia666 <diegogarcia666@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-05-24 16:50:36 +0000

    Agregardo de jdom para el parser. Aun está en desarrollo. Saludos

---
+++ TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/util/XMLParser.java
@@ -0,0 +1,9 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.util;
+import com.sun.xml.*;
+import java.util.*;
+import ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.productos.Producto;
+
+public class XMLParser {
+	public ArrayList<Producto> LeerDoc(String path){
+		return null;}
+}
