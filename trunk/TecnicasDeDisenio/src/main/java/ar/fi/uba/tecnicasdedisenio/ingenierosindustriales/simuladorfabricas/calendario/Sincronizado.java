134412b54d86c59086e32d233467827149955a74
invernizzie <invernizzie@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-05-24 06:11:01 +0000

    Solucion al problema del calendario y el paso del tiempo. Faltan todos los tests! Pero tengo sueño, mañana los hago...

---
+++ trunk/TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/calendario/Sincronizado.java
@@ -0,0 +1,9 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.calendario;
+
+/**
+ * @author Esteban I. Invernizzi (invernizzie@gmail.com)
+ * @created 24/05/2010
+ */
+public interface Sincronizado {
+    void notificar(Evento evento);
+}
