23875f0126a1541467c447a3b96bf6079998241b
invernizzie <invernizzie@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-05-29 21:48:00 +0000

    Calendario corregido y con sus tests. No servía el thread como clase interna no estática, los threads nunca se destruían porque la instancia de la clase host seguía existiendo.

---
+++ TecnicasDeDisenio/src/test/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/tests/calendario/ContadorSincronizado.java
@@ -0,0 +1,22 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.tests.calendario;
+
+import ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.calendario.Evento;
+import ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.calendario.Sincronizado;
+
+/**
+ * @author Esteban I. Invernizzi (invernizzie@gmail.com)
+ *         Date: 29/05/2010
+ */
+public class ContadorSincronizado implements Sincronizado {
+
+    public int notificacionesDiarias = 0;
+    public int notificacionesSemanales = 0;
+
+    @Override
+    public void notificar(Evento evento) {
+        if (evento == Evento.COMIENZO_DE_DIA)
+            notificacionesDiarias++;
+        else if (evento == Evento.COMIENZO_DE_SEMANA)
+            notificacionesSemanales++;
+    }
+}
