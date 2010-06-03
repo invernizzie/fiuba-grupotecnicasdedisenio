83a74a5c53f6c66fc9d4ae462b3da20a2f70e704
gmeller <gmeller@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-06-03 15:44:42 +0000

    Compra, venta y alquiler de fabricas.

---
+++ TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/DineroInsuficienteException.java
@@ -0,0 +1,5 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas;
+
+public class DineroInsuficienteException extends Exception {
+
+}
--- TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/Fabrica.java
+++ TecnicasDeDisenio/src/main/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/Fabrica.java
@@ -13,11 +13,22 @@ public class Fabrica {
 	private List<Maquina> maquinas;
 	private List<LineaProduccion> lineas;
 	private List<Fuente> fuentes;
+	private float costoCompra;
+	private float costoAlquiler;
+	private int metrosCuadrados;
+	private int cantMaximaLineas;
+	private Jugador jugador;
+	private float costoFabricaXMes;
 
-	public Fabrica(int ancho, int largo) {
+	public Fabrica(int metrosCuadrados, float costoCompra, float costoAlquiler, int cantMaximaLineas) {
 		this.maquinas = new ArrayList<Maquina>();
 		this.lineas = new ArrayList<LineaProduccion>();
 		this.fuentes = new ArrayList<Fuente>();
+		this.setMetrosCuadrados(metrosCuadrados);
+		this.setCostoCompra(costoCompra);
+		this.setCostoAlquiler(costoAlquiler);
+		this.setCantMaximaLineas(cantMaximaLineas);
+		this.setCostoFabricaXMes(0);
 	}
 
 	public void agregarFuente(Fuente fuente) {
@@ -87,5 +98,148 @@ public class Fabrica {
 		}
 		
 	}
+	
+	public void agregarLinea(LineaProduccion linea) throws CantidadLineasMaximaException {
+		this.verificarCantidadLineas();
+		this.lineas.add(linea);
+	}
+	
+	public void verificarCantidadLineas() throws CantidadLineasMaximaException{
+		if(this.lineas.size()==this.getCantMaximaLineas())
+			throw new CantidadLineasMaximaException();
+		
+	}
+	
+	public void setCostoCompra(float costoCompra) {
+		this.costoCompra = costoCompra;
+	}
 
+	public float getCostoCompra() {
+		return costoCompra;
+	}
+
+	public void setCostoAlquiler(float costoAlquiler) {
+		this.costoAlquiler = costoAlquiler;
+	}
+
+	public float getCostoAlquiler() {
+		return costoAlquiler;
+	}
+
+	public void setMetrosCuadrados(int metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+
+	public int getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+
+	public void setCantMaximaLineas(int cantMaximaLineas) {
+		this.cantMaximaLineas = cantMaximaLineas;
+	}
+
+	public int getCantMaximaLineas() {
+		return cantMaximaLineas;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+	
+	public void setCostoFabricaXMes(float costoFabricaXMes) {
+		this.costoFabricaXMes = costoFabricaXMes;
+	}
+
+	public float getCostoFabricaXMes() {
+		return costoFabricaXMes;
+	}
+	
+	/**
+	 * Verifica la existencia de un jugador poseyendo la fábrica.
+	 * @throws FabricaOcupadaException
+	 */
+	public void verificarJugadorAsignado() throws FabricaOcupadaException{
+		if(this.getJugador()!=null)
+			throw new FabricaOcupadaException();
+	}
+	
+	/**
+	 * La fábrica se intenta comprar, previamente verificando que se cumplan las condiciones de
+	 * dinero suficiente.
+	 * @param jugador
+	 * @throws DineroInsuficienteException
+	 * @throws FabricaOcupadaException
+	 * @throws JugadorConFabricaException
+	 */
+	public void comprar(Jugador jugador) throws DineroInsuficienteException, FabricaOcupadaException, JugadorConFabricaException{
+		jugador.verificarDineroSuficiente(this.getCostoCompra());
+		this.realizarCompraOAlquiler(jugador,0);
+		jugador.comprarFabrica(this);
+	}
+	
+	/**
+	 * La fábrica se intenta alquilar.
+	 * @param jugador
+	 * @throws FabricaOcupadaException
+	 * @throws JugadorConFabricaException
+	 */
+	public void alquilar(Jugador jugador) throws FabricaOcupadaException, JugadorConFabricaException{
+		this.realizarCompraOAlquiler(jugador,this.getCostoAlquiler());
+		jugador.alquilarFabrica(this);
+	}
+	
+	/**
+	 * Realiza la compra o alquiler de la fábrica verificando que el jugador no tenga
+	 * ya una fábrica asignada y que la fábrica no este asignada a un jugador.
+	 * @param jugador
+	 * @param costoXMes
+	 * @throws FabricaOcupadaException
+	 * @throws JugadorConFabricaException
+	 */
+	public void realizarCompraOAlquiler(Jugador jugador, float costoXMes) throws FabricaOcupadaException, JugadorConFabricaException{
+		jugador.verificarFabricaAsignada();
+		this.verificarJugadorAsignado();
+		this.setJugador(jugador);
+		this.setCostoFabricaXMes(costoXMes);
+	}
+
+	/**
+	 * La fábrica deja de tener un jugador asignado.
+	 */
+	public void vender(){
+		try{
+			this.verificarJugadorAsignado();
+		}
+		catch(FabricaOcupadaException e){
+			if(this.isAlquilada())
+				this.getJugador().venderFabrica(0);
+			this.getJugador().venderFabrica((float) (this.getCostoCompra()*0.8));
+		}
+		this.setCostoFabricaXMes(0);
+		this.setJugador(null);
+		
+		/*Deberia borrar todas sus lineas de produccion y tambien pasarle el 50%
+		 * del valor de cada maquina que no este rota al jugador.*/
+	}
+	
+	/**
+	 * Si esta alquilada el costo x mes es el costo de alquiler y tiene un jugador asignado.
+	 * @return
+	 */
+	public boolean isAlquilada(){
+		return (this.getCostoAlquiler()==this.getCostoFabricaXMes() && this.getJugador()!=null);
+	}
+	
+	/**
+	 * Si esta comprada el costo x mes es nulo y tiene un jugador asignado.
+	 * @return
+	 */
+	public boolean isComprada(){
+		return (this.getCostoFabricaXMes()==0 && this.getJugador()!=null);
+	}
+	
 }
