2579f0cfdae36c6f68d59beeb98b5aa2483e64ce
diegogarcia666 <diegogarcia666@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-06-06 15:56:00 +0000

    Test de Area Fabrica

---
+++ trunk/TecnicasDeDisenio/src/test/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/interfazgrafica/TestAreaFabrica.java
@@ -0,0 +1,29 @@
+package ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.tests.interfazgrafica;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.fi.uba.tecnicasdedisenio.ingenierosindustriales.simuladorfabricas.interfazgrafica.*;
+
+
+public class TestAreaFabrica {
+	private AreaFabrica ventana;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.ventana = new AreaFabrica();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPantalla() {
+		this.ventana.run();
+		}
+}
