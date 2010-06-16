dfd77374c6bb173b17ab3807bbc37428eea72d1f
gmeller <gmeller@be1df2d7-dd68-dd3b-24e5-eaf5fce3b33a>
2010-06-16 22:13:24 +0000

    Correccion de un test q fallaba.

--- trunk/TecnicasDeDisenio/src/test/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/tests/productos/TestProductos.java
+++ trunk/TecnicasDeDisenio/src/test/java/ar/fi/uba/tecnicasdedisenio/ingenierosindustriales/simuladorfabricas/tests/productos/TestProductos.java
@@ -31,6 +31,6 @@ public class TestProductos {
 	@Test
 	public void testEqualsProducto() {
 		Producto prod = new Producto(val, "pan",0);
-		Assert.assertEquals("Error en el estado de Producto",producto, prod);
+		Assert.assertEquals("Error en el estado de Producto",producto.getEstado(), prod.getEstado());
 	}
 }
